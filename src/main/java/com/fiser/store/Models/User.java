55fef12f69de6cf98e00fd6d01a534f1c109ba53
Fiser04 <vf6a@seznam.cz>
2026-08-29 23:14:10 +0200

    add explicit column name for User entity ID field

--- src/main/java/com/fiser/store/Models/User.java
+++ src/main/java/com/fiser/store/Models/User.java
@@ -15,6 +15,7 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(nullable = false, name = "name")
