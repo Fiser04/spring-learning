d6f985d53fd030c31662884183893bbed5972394
Fiser04 <vf6a@seznam.cz>
2026-08-29 21:54:16 +0200

    add MySQL datasource configuration, JPA, and Flyway dependencies; create initial migration script for users and addresses tables

--- src/main/java/com/fiser/store/StoreApplication.java
+++ src/main/java/com/fiser/store/StoreApplication.java
@@ -7,16 +7,17 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class StoreApplication {
 
-//    public static void main(String[] args) {
-//        SpringApplication.run(StoreApplication.class, args);
-//    }
     public static void main(String[] args) {
-        ApplicationContext applicationContext = SpringApplication.run(StoreApplication.class, args);
-        var orderService = applicationContext.getBean(OrderService.class);
-        var notificationManager = applicationContext.getBean(NotificationManager.class);
-        notificationManager.notify("Your order has been placed successfully!");
-        orderService.placeOrder();
-        var heavyResource = applicationContext.getBean(HeavyResource.class);
+        SpringApplication.run(StoreApplication.class, args);
     }
+//    public static void main(String[] args) {
+//        ApplicationContext applicationContext = SpringApplication.run(StoreApplication.class, args);
+//        var orderService = applicationContext.getBean(OrderService.class);
+//        var notificationManager = applicationContext.getBean(NotificationManager.class);
+//        notificationManager.notify("Your order has been placed successfully!");
+//        orderService.placeOrder();
+//        var heavyResource = applicationContext.getBean(HeavyResource.class);
+//
+//    }
 
 }
