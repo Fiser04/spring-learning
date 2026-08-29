ae2910dae5920e80985b48fb565947ffa54e049d
Fiser04 <vf6a@seznam.cz>
2026-08-29 22:56:21 +0200

    add Lombok dependency and refactor User entity to use Lombok annotations

--- src/main/java/com/fiser/store/Models/User.java
+++ src/main/java/com/fiser/store/Models/User.java
@@ -1,8 +1,16 @@
 package com.fiser.store.Models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
     @Id
@@ -17,38 +25,4 @@ public class User {
 
     @Column(nullable = false, name = "password")
     private String password;
-
-    // Getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
